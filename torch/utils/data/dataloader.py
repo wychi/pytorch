f7a4af77db81e8fe378648f876cd1ffd5c7cd4ea
Divyansh Khanna <divyanshkhanna@meta.com>
2025-07-30 11:17:28 -0700

    Update arg name

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -16,7 +16,6 @@ import queue
 import threading
 import warnings
 from typing import Any, Callable, Generic, Optional, TYPE_CHECKING, TypeVar, Union
-from typing_extensions import Self
 
 import torch
 import torch.distributed as dist
@@ -36,6 +35,7 @@ from torch.utils.data.sampler import (
     Sampler,
     SequentialSampler,
 )
+from typing_extensions import Self
 
 
 if TYPE_CHECKING:
@@ -1175,13 +1175,13 @@ class _MultiProcessingDataLoaderIter(_BaseDataLoaderIter):
 
             # Queue is not type-annotated
             self._data_queue = queue.Queue()  # type: ignore[var-annotated]
-            current_device = torch.accelerator.current_device_index()
+            current_device_id = torch.accelerator.current_device_index()
             pin_memory_thread = threading.Thread(
                 target=_utils.pin_memory._pin_memory_loop,
                 args=(
                     self._worker_result_queue,
                     self._data_queue,
-                    current_device,
+                    current_device_id,
                     self._pin_memory_thread_done_event,
                     self._pin_memory_device,
                 ),
